01bac420ba689d8371b52b126286c690f886ee0c
Shub1ham <shubhamkokate5554@gmail.com>
2025-02-19 02:03:25 +0530

    light ans dark

---
+++ 7-Light_and_dark_mode/lightanddark/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Index from "./component/Index";
+
+function App() {
+  return  <Index></Index>
+}
+
+export default App;
---
+++ 7-Light_and_dark_mode/lightanddark/src/component/Index.jsx
@@ -0,0 +1,11 @@
+function Index(){
+   return(
+    <div>
+        <div>
+            <p>Hello World !</p>
+            <button>Change Theme</button>
+        </div>
+    </div>
+   )
+}
+export default Index
