e60c1f7eb15a9580ed5d2ab2634d4cdc7a615101
Shub1ham <shubhamkokate5554@gmail.com>
2025-02-19 05:10:45 +0530

    newsapp

---
+++ 8-NewsApp/newsapp/src/Components/Newsboard.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Newsitem from "./Newsitem";
+
+function Newsboard(){
+    const [articles,setarticles]=useState([])
+    useEffect(()=>{
+        fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=cc0f4dfd0458461f9f71b56c798ddb99")
+        .then(response=>response.json())
+        .then(data=>setarticles(data.articles))
+    },[])
+    return(
+        <div>
+            <h1 className="text-center">Latest <span className="badge bg-danger">News</span></h1>
+            {articles.map((news,index)=>{
+                return <Newsitem key={index} title={news.title} description={news.description} 
+                src={news.urlToImage} url={news.url}></Newsitem>
+            })}
+        </div>
+    );
+}
+
+export default Newsboard;
